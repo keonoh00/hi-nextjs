a89e837cbcb565732276343d2dd8d38abce96578
keonoh00 <keonoh00@gmail.com>
2024-11-04 15:31:08 +0900

    Simple request and response example

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,12 +1,28 @@
-export const metadata = {
-  title: "Home",
-  description: "Developed by Keon Oh Kim",
-};
+"use client";
+
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [movies, setMovies] = useState();
+
+  const getMovies = async () => {
+    const response = await fetch(
+      "https://nomad-movies.nomadcoders.workers.dev/movies"
+    );
+    const responseJson = await response.json();
+    setMovies(responseJson);
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
   return (
     <div>
       <h1>Hello NextJS</h1>
+      {isLoading ? "Loading..." : JSON.stringify(movies)}
     </div>
   );
 }
