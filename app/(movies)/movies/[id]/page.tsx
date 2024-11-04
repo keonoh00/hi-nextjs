e481f2350882d266aa44531ecf63d35db95f8055
keonoh00 <keonoh00@gmail.com>
2024-11-04 15:01:05 +0900

    Handle possible param lost in async handling

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,7 +1,5 @@
-export default function MovieDetails({
-  params: { id },
-}: {
-  params: { id: string };
-}) {
-  return <h2>This is movie detail with id: {id}</h2>;
+export default async function MovieDetails(props) {
+  const param = await props;
+
+  return <h2>This is movie detail with id: {param.id}</h2>;
 }
