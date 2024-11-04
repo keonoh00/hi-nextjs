5758c4972740a30008a7f789ecb6aa2f3f1f7c36
keonoh00 <keonoh00@gmail.com>
2024-11-04 20:07:55 +0900

    Example of using Suspense on server side

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,29 +1,21 @@
-import { API_BASE_URL } from "../../../(home)/page";
-
-async function getMovie(id: string) {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  const response = await fetch(`${API_BASE_URL}/${id}`);
-
-  return response.json();
-}
-
-async function getMovieVideos(id: string) {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  const response = await fetch(`${API_BASE_URL}/${id}/videos`);
-  return response.json();
-}
+import { Suspense } from "react";
+import MovieInfo from "../../../../components/MovieInfo";
+import MovieVideos from "../../../../components/MovieVideos";
 
 export default async function MovieDetails({
   params: { id },
 }: {
   params: { id: string };
 }) {
-  const [movie, videos] = await Promise.all([getMovie(id), getMovieVideos(id)]);
-
   return (
     <div>
-      <h2>Title: {movie.title}</h2>
-      <p>{JSON.stringify(videos)}</p>
+      <h3>Movie detail page</h3>
+      <Suspense fallback={<h1>Loading movie info</h1>}>
+        <MovieInfo id={id} />
+      </Suspense>
+      <Suspense fallback={<h1>Loading movie videos</h1>}>
+        <MovieVideos id={id} />
+      </Suspense>
     </div>
   );
 }
